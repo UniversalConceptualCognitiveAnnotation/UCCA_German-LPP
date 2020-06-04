4f582f4b168d5312a194381194e53f3b9ad37315
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-06-04 11:01:46 +0200

    Fix detokenization

--- scripts/replacements.sh
+++ scripts/replacements.sh
@@ -1,3 +1,3 @@
-perl -pe 's/\r?\n/ /' kleine_prinz.nicole_corrected.preprocessed.txt | perl -pe 's/``/"/g;s/'"''"'/"/g;s/--/-/g' > LPP_German_oneline.txt.
+perl -pe 's/\r?\n/ /' kleine_prinz.nicole_corrected.preprocessed.txt | perl -pe 's/\( /(/g;s/ ([),.:?!])/\1/g;s/`` /"/g;s/ '"''"'/"/g;s/--/-/g' > LPP_German_oneline.txt.
 cat LPP_German_oneline.txt. unmatched_sentences.txt > LPP_German_oneline.txt
 rm LPP_German_oneline.txt.
